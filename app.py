ca76d7db9ac6243dfb657267217e3b7a8d97df03
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2026-01-21 00:21:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from routes import character_bp
 
 app = Flask(__name__)
+#le indica a flask que el conte4xto actual existe. documentacion de flask
 
 #CRUD
 # create read update delete
 
-
+#esto va despues
 app.register_blueprint(character_bp)
 
+#esto si va al inicio
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
